ba8676f2f3ca63cc6063fd713dd0fba05fe34d45
Andrey <andrei.positko.gb@gmail.com>
2020-10-29 17:19:19 +0200

    create function - get data from server

---
+++ src/scripts/modules/template.js
@@ -0,0 +1,27 @@
+export const template = `
+<a href="#">
+              <div class="books__buttons">
+                <button class="btn">Удалить</button>
+                <button class="btn">Изменить</button>
+              </div>
+              <div class="books__wrapper">
+                <img class="book__img" src="img/algoritm.jpg" alt="">
+                <div class="books__info">
+                  <p class="title__book"><b>Название:</b> Теория Алгоритмов</p>
+                  <p class="book__author"><b>Автор:</b> Буэно Вальковский</p>
+                  <p class="book__pages"><b>Страниц:</b> 225</p>
+                  <!-- <p class="book__size"><b>Размер:</b> 125MB</p> -->
+                  <p class="book__quality"><b>Качество:</b> Отличное</p>
+                  <p class="book__language"><b>Язык:</b> Английский</p>
+                  <p class="publishing__date"><b>Год издания:</b> 2015</p>
+                  <div class="block__description">
+                    <b>
+                      <p>Описание:</p>
+                    </b>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit blanditiis at animi iure deleniti
+                      quisquam deserunt
+                      porro ipsa sit voluptatem a cupiditate odio, impedit asperiores earum cum dolores amet quis.</p>
+                  </div>
+                </div>
+              </div>
+            </a>`;
