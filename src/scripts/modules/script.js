d50c1f439c5102c2474ab7b5854d862f77eea061
Andrey <andrei.positko.gb@gmail.com>
2020-10-29 17:45:59 +0200

    create all necessary elements

--- src/scripts/modules/script.js
+++ src/scripts/modules/script.js
@@ -8,6 +8,30 @@ let overlay = document.querySelector('.overlay');
 let btnEdit = document.querySelector('.popup__btn_edit');
 let inputs = document.querySelectorAll('input');
 
+/** function - dreate element */
+
+function createElement(tagName, className) {
+    const element = document.createElement(tagName);
+    element.classList = className;
+    return element;
+}
+
+const bookLink = createElement('a', 'book__link');
+const booksButtons = createElement('div', 'books__buttons');
+const btn = createElement('button', 'btn');
+const booksWrapper = createElement('div', 'books__wrapper');
+const bookImg = createElement('img', 'book__img');
+const booksInfo = createElement('div', 'books__info');
+const titleBook = createElement('p', 'title__book');
+const bookAuthor = createElement('p', 'book__author');
+const bookPages = createElement('p', 'book__pages');
+const bookQuality = createElement('p', 'book__quality');
+const bookLanguage = createElement('p', 'book__language');
+const publishingDate = createElement('p', 'publishing__date');
+const blockDescription = createElement('div', 'block__description');
+const descriptionName = createElement('p', 'description__name');
+const descriptionText = createElement('p', 'description__text');
+
 /** Show overlay */
 
 btnAddBook.onclick = function () {
@@ -50,6 +74,10 @@ let loadBooks = () => {
             return responce.json();
         })
         .then(function (data) {
+            const ul = document.querySelector('.popup__list');
+            const html = data.map(function (item) {
+                return '<li></li>'
+            })
             console.log(data);
         });
 };
--- src/scripts/modules/template.js
+++ src/scripts/modules/template.js
@@ -1,5 +1,5 @@
 export const template = `
-<a href="#">
+<a class="book__link" href="#">
               <div class="books__buttons">
                 <button class="btn">Удалить</button>
                 <button class="btn">Изменить</button>
@@ -10,15 +10,14 @@ export const template = `
                   <p class="title__book"><b>Название:</b> Теория Алгоритмов</p>
                   <p class="book__author"><b>Автор:</b> Буэно Вальковский</p>
                   <p class="book__pages"><b>Страниц:</b> 225</p>
-                  <!-- <p class="book__size"><b>Размер:</b> 125MB</p> -->
                   <p class="book__quality"><b>Качество:</b> Отличное</p>
                   <p class="book__language"><b>Язык:</b> Английский</p>
                   <p class="publishing__date"><b>Год издания:</b> 2015</p>
                   <div class="block__description">
                     <b>
-                      <p>Описание:</p>
+                      <p class="description__name">Описание:</p>
                     </b>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit blanditiis at animi iure deleniti
+                    <p class="description__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit blanditiis at animi iure deleniti
                       quisquam deserunt
                       porro ipsa sit voluptatem a cupiditate odio, impedit asperiores earum cum dolores amet quis.</p>
                   </div>
